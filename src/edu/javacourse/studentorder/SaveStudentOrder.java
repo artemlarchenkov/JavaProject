fd0326b035d1e2107b20d4bee6ffd157cabadff0
artemlarchenkov <artem.bond.2000@mail.ru>
2023-12-16 21:10:55 +0300

    New update

--- src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -8,8 +8,6 @@ public class SaveStudentOrder
 //    scheduleStudentOrder
 //    FinanceStudentOrder
     public static void main(String[] args) {
-        buildStudentOrder();
-
 //        StudentOrder so = new StudentOrder();
 //        long ans = saveStudentOrder(so);
 //        System.out.println(ans);
@@ -22,18 +20,9 @@ public class SaveStudentOrder
         return answer;
     }
 
-    static StudentOrder buildStudentOrder() {
+    static StudentOrder buildStudentOrder(long id) {
         StudentOrder so = new StudentOrder();
-
-
-        Adult husband = new Adult();
-//        husband.setGivenName("Андрей");
-//        husband.setSurName("Петров");
-//        husband.setPassportNumber("123456");
-//        so.setHusband(husband);
-//
-//        String ans = husband.getPersonString();
-//        System.out.println(ans);
+        so.setStudentOrderId(id);
 
         return so;
     }
